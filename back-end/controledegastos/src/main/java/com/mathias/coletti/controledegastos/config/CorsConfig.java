65d0ac71809a54bbf4901eef570d3facf37c235c
mathiascoletticomunhao <mathias.coletti@comunhaoespirita.org.br>
2026-08-29 10:56:35 -0300

    back

--- back-end/controledegastos/src/main/java/com/mathias/coletti/controledegastos/config/CorsConfig.java
+++ back-end/controledegastos/src/main/java/com/mathias/coletti/controledegastos/config/CorsConfig.java
@@ -8,19 +8,19 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-//@Configuration
+@Configuration
 public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        // Origens permitidas do Front-end
+        // Origens permitidas (incluindo o seu front-end em produção no Render e as portas locais)
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
                 "http://localhost:8080",
-                "https://controle-de-gastos-15x8.onrender.com"
+                "https://controle-de-gastos-front.onrender.com"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
